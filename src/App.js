bee43b0469c5a86d5606e5f6793d240e8e8dac37
Ashish Bhutiani <ashishterp@gmail.com>
2021-07-21 12:02:29 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -17,63 +17,22 @@ function App() {
           target="_blank"
           rel="noreferrer"
         >
-          Here's Our Registry!
-        </a>
-      </div>
-      <div className="col-span-4 lg:col-span-2 lg:col-start-2 mx-auto p-8">
-        <a
-          href="https://www.signupgenius.com/go/30E0A4DA5AA2BAB9-its"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="https://www.signupgenius.com/images/sign-up-now2.gif"
-            width="250"
-            height="45"
-            border="0"
-            alt="Sign Up!"
-          />
+          Registry!
         </a>
       </div>
 
       <div className=" rounded-md col-span-4 opacity-75 lg:col-span-2 lg:col-start-2 mx-auto p-4 bg-white shadow-md">
+        <p className="mb-4">Dear Friends-</p>
         <p className="mb-4">
-          Because our favorite people are located all over and have insane
-          schedules, it's actually several virtual showers on Zoom — you have a
-          few days from which to choose.
-          <span class="font-bold">Use the Sign Up button above to RSVP.</span>
-        </p>
-        <p className="mb-4 italic">
-          (Please try to sign in from one device. If you know you'll be signing
-          in from more than one (e.g., your spouse is traveling), let us know so
-          we can ensure you get two slots.)
+          We just had to share this news with you personally. Well, as
+          personally as a mass message feels. Basically, we couldn't let you
+          just find out on Facebook.
         </p>
         <p className="mb-4">
-          The showers will be like us: short & sweet. 30 minutes. Rajiv will do
-          some standup and Harsha will get a word in edgewise at some point.
-        </p>
-        <p className="mb-4">
-          We won't play any games but we do wanna hear if anyone has any boy
-          name suggestions.
-        </p>
-        <span class="font-bold">Parameters:</span>
-        <ul className="list-disc list-inside mb-4">
-          <li>Indian.</li>
-          <li>Easy for the whites to pronounce.</li>
-          <li>Sounds good with "Satyal."</li>
-          <li>Short version is cool.</li>
-          <li>Works as a kid and as an adult.</li>
-          <li>Doesn't have to be too religious.</li>
-          <li>
-            And not too New Agey either.We dislike most names, and if your kid's
-            is too New Agey, we're preventing our kid from playing with yours.
-          </li>
-        </ul>
-        <p className="mb-4">
-          This may come as a surprise since Rajiv's so public, but since
-          Harsha's so private (Wouldn't you be if you married Rajiv?), we're not
-          sharing this news broadly, on social media, etc. This is a special
-          event and we're inviting you, our friends.
+          Speaking of which, this may come as a surprise since Rajiv's so
+          public, but since Harsha's so private (Wouldn't you be if you married
+          Rajiv?), we're not sharing this news on social media until our baby
+          boy actually arrives, hopefully on 12 August.
         </p>
         <p className="mb-4">
           All kidding aside, thank you for sharing this moment with us. We're
